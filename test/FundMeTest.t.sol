19d11f045e2492ce2a26f521c85a140a17f45b9a
elian <elianmars13@gmail.com>
2025-03-18 14:42:32 +0300

    mock

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -8,11 +8,15 @@ import {DeployFundMe} from "../script/DeployFundMe.s.sol";
 
 contract FundMeTest is Test {
     FundMe fundMe;
+    address USER = makeAddr("user");
+    uint256 constant SEND_VALUE = 10 ether;
+    uint256 constant STARTING_BALANCE=20 ether;
 
     function setUp() external {
         //fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
         DeployFundMe deployFundMe = new DeployFundMe();
         fundMe = deployFundMe.run();
+        vm.deal(USER,STARTING_BALANCE);
     }
 
     function testMinimumDollarIsFive() public view {
@@ -24,4 +28,22 @@ contract FundMeTest is Test {
         console.log(address(this));
         assertEq(fundMe.i_owner(), msg.sender);
     }
+
+    function testPriceFeedVersionIsAccurate() public view{
+        uint256 version=fundMe.getVersion();
+        assertEq(version,4);
+    }
+
+    function testFundFailsWithoutEnoughETH() public {
+        vm.expectRevert();
+        fundMe.fund();
+    }
+
+    function testFundUpdatesFundDataStructure() public {
+    vm.prank(USER); //the next transaction will be sent by USER
+    fundMe.fund{value: SEND_VALUE}();
+    uint256 amountFunded = fundMe.getAddressToAmountFunded(USER);
+    assertEq(amountFunded, SEND_VALUE);
+
+}
 }
